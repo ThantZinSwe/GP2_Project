5c85d8affca55d3b0e8cd34ef28b9e601ea5c17f
HninLwin <hninmohmohlwin16gmail.com>
2022-11-06 21:56:48 +0630

    added home Responsive Fix

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -5,7 +5,7 @@
     <div class="home-tran"></div>
     <div class="l-inner clearfix home-sfirst">
       <div class="left-blk">
-        <h3 class="cmn-title">Welcome to <br/><span class="title">CoderCamp</span></h3>
+        <h3 class="cmn-title">Welcome to <span class="title">CoderCamp</span></h3>
         <p class="cmn-paragraph">Courses and blogs about Programming Languages. Intended to whom that are interested in App and Web Development and Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus 
           
           </p>
@@ -40,34 +40,79 @@
           </div>
         </li>
     </ul>
-      </div>
-      <a href="#home-sec" class="scroll-icon" id="scroll-icon"><i class="fa-solid fa-angle-up"></i></a>
-    </section>
-    <!--<section class="course">
-      <ul class="course-list clearfix">
-        @foreach ($courses as $data)
-        <li class="course-card">
-          <a href="#">
-              <img src="{{asset('images/course/'.$data->image)}}" alt="course-image" class="course-card-img">
-              <div class="course-txt-blk">
-                  <h3 class="course-card-ttl">{{ $data->name }}</h3>
-                  <p class="course-card-type">Type: <span>{{ $data->type }}</span></p>
-                  <p class="course-card-fee">Fee: <span>{{ $data->price }}Ks</span></p>
-                  <ul class="course-card-language">
-                    @foreach ($data->languages as $language)
-                    <li>{{ $language->name }}</li> 
+    </div>
+    <section class="course">
+      <div class="l-inner home-course">
+     
+            
+        <ul class="card clearfix">
+
+                    @foreach ($courses as $item)
+
+                    <li class="card-list">
+                        <a href="{{route('user.courseDetails',$item->slug)}}">
+                            <div class="card-img">
+                                <img src="{{asset('images/course/'.$item->image)}}" alt="">
+                                <div class="type">
+                                    <span>{{$item->type}} !</span>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <p class="name">{{$item->name}}</p>
+                                <p class="create-time"><i class="fa-solid fa-calendar-day"></i> Created At - {{Carbon\Carbon::parse($item->created_at)->format('Y-m-d')}}</p>
+
+                                <p class="course-video"><i class="fa-solid fa-video"></i> Videos - <span>{{$item->courseVideos->count()}}</span></p>
+                                <br>
+
+                                <p class=" about-course">
+                                    {{$item->description}}
+                                </p><br>
+
+                                <div class="language">
+                                    @foreach ($item->languages as $language)
+                                        <span class="badge">{{$language->name}}</span>
+                                    @endforeach
+                                </div>
+
+                                <div class="price{{$item->price == 0 ? '-zero' : ''}}">
+                                    <span>{{$item->price}} Ks</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
                     @endforeach
-                  </ul>
-              </div>
-          </a>
-      </li> 
-        @endforeach
-      </ul>
-</section>-->
-      <!--<div class="course-btn">
-        <a href="{{route('user.course')}}" class="btn btn-primary">See All Courses</a>
+                </ul>
+        <a href="{{route('user.course')}}" class="home-cbtn">See All Courses</a>
       </div>
-    </section>-->
+  </section>
+      <a href="#home-sec" class="scroll-icon" id="scroll-icon"><i class="fa-solid fa-angle-up"></i></a>
+</section>
+   <section class="home-bene">
+    <div class="l-inner home-betitl">
+      <h4>Awsome Feature</h4>
+      <p class="be-titl">Some Benefits of attending our courses...</p>
+      <ul class="clearfix be-list">
+        <li class="home-belist">
+        <i class="fa-solid fa-graduation-cap"></i>
+        <h5>On the job training</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ea aspernatur hic illo voluptate tempora possimus dolorem </p>
+        </li>
+        <li class="home-belist">
+        <i class="fa-solid fa-video"></i>
+        <h5>Sell Online Course</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ea aspernatur hic illo voluptate tempora possimus dolorem </p>
+        </li>
+        <li class="home-belist">
+        <i class="fa-solid fa-certificate"></i>
+        <h5>Certification</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ea aspernatur hic illo voluptate tempora possimus dolorem </p>
+        </li>
+      </ul>
+    </div>
+
+   </section> 
+      
     
     <section class="home-bsec">
     
